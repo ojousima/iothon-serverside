e7154a108e0667fdd8f480f0ec55e9643393dba7
root <root@flash-entry-playground.localdomain>
2018-01-18 15:41:19 +0000

    Add IOTA transfers

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-var express = require('express')
+const express = require('express')
  , http = require('http')
  , bodyParser = require('body-parser')
- , endpoints = require('ruuvi.endpoints.js');
+ , endpoints = require('ruuvi.endpoints.js')
+ , IOTA = require('iota.lib.js');
+
+// not really used as we don't do any TX with value changes.
+const MWM = 15;
+const seed = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ9ABCDEFGHIJKLMNOPQRSTUVWXYZ9ABCDEFGHIJKLMNOPQRSTUVWXYZ9';
+const targetAddress = '999999999999999999999999999999999999999999999999999999999999999999999999999IOTHON';
+var targetTag = "9999HELLOIOTAFROMIOTHON9999"
+var depth = 1;
+const options = {}; //Empty
+
+var iota = new IOTA({
+  host: "http://iri1.iota.fm:80"
+});
+
+iota.api.getNodeInfo((error, nodeInfo) => {
+	console.log("Querying");
+    if (error) {
+        console.error('getNodeInfo error', error);
+    } else {
+        console.log('getNodeInfo result', nodeInfo);
+    }
+});
+
+var lock = false;
 
 var app = express();
 
@@ -9,6 +33,11 @@ app.set('port', process.env.PORT || 5375);
 
 app.use(express.static(__dirname + '/public/images'));
 app.use(bodyParser.json({limit:1024102420, type:'application/json'}));
+
+var unlock = function(){
+  console.log("Unlocking");
+  lock = false;
+}
 
 app.post('/data/',function(request,response,next){
 /*
@@ -37,12 +66,33 @@ app.post('/data/',function(request,response,next){
   let arrayLength = samples.length;
   for (let ii = 0; ii < arrayLength; ii++) {
   	//Get data from each sample. 
-    if(samples[ii].rawData && samples[ii].rawData.length > 12 ){
+    if(!lock && samples[ii].rawData && samples[ii].rawData.length > 12 ){
+    	lock = true;
       //console.log(samples[ii].rawData);
-      const buf = Buffer.from(samples[ii].rawData, 'hex');
-      latestSample = endpoints.parse(buf.slice(7));
+      //const buf = Buffer.from(samples[ii].rawData, 'hex');
+      //latestSample = endpoints.parse(buf.slice(7));
       //console.log(buf.slice(8).toString('hex'));
-      console.log(JSON.stringify(latestSample));
+      //console.log(JSON.stringify(latestSample));
+      console.log(samples[ii]);
+      let trytedata = iota.utils.toTrytes(JSON.stringify(samples[ii]));
+      console.log(trytedata);
+      let transfer = {
+      	address: targetAddress,
+        value: 0,
+        message: trytedata,
+        tag: targetTag
+      };
+      let transfers = [transfer];
+      iota.api.sendTransfer(seed, depth, MWM, transfers, options, (error, transactions) => {
+        if (error) {
+          console.error('sendTransfer error', error);
+          lock = false;
+        } else {
+          console.log('transactions sent!', transactions);
+          lock = false;
+        }
+      });
+      console.log("Transfer in pipe");
     }
   }
   response.send('hello world');
